8c8ca4a4ca913f8568cc284372a1c874add7ab86
sbx320@sbx320.net <sbx320@sbx320.net@f49503af-255e-f743-15ea-90d821dd2f5c>
2013-08-17 23:52:07 +0000

    Fixed HUD elements trying to draw before being downloaded

--- open_dayz/client/classes/HUD/HUDHunger.lua
+++ open_dayz/client/classes/HUD/HUDHunger.lua
@@ -9,8 +9,21 @@ HUDHunger = inherit(HUDElement)
 
 function HUDHunger:constructor(posX, posY, width, height, parent)
 	HUDElement.constructor(self, posX, posY, width, height, parent)
+	TransferManager:getSingleton():requestFilesAsOnce(
+	{	"files/images/HUD/Hunger.png";
+	}
+	, bind(HUDHunger.load, self))
+	
+	self.m_Active = false
 end
 
 function HUDHunger:drawThis()
-	dxDrawImage(self.m_AbsoluteX, self.m_AbsoluteY, self.m_Width, self.m_Height, "files/images/HUD/Hunger.png", 0, 0, 0, tocolor(255-255*(localPlayer:getHunger()/100), 255*(localPlayer:getHunger()/100), 0))
+	if self.m_Active then
+		dxDrawImage(self.m_AbsoluteX, self.m_AbsoluteY, self.m_Width, self.m_Height, "files/images/HUD/Hunger.png", 0, 0, 0, tocolor(255-255*(localPlayer:getHunger()/100), 255*(localPlayer:getHunger()/100), 0))
+	end
 end
+
+function HUDHunger:load()
+	self.m_Active = true
+end
+
--- open_dayz/client/classes/HUD/HUDThirst.lua
+++ open_dayz/client/classes/HUD/HUDThirst.lua
@@ -5,12 +5,24 @@
 -- *  PURPOSE:     Hunger HUD element class
 -- *
 -- ****************************************************************************
-HUDThirst = inherit(HUDElement)
+HUDThirst = inherit(HUDThirst)
 
 function HUDThirst:constructor(posX, posY, width, height, parent)
 	HUDElement.constructor(self, posX, posY, width, height, parent)
+	TransferManager:getSingleton():requestFilesAsOnce(
+	{	"files/images/HUD/Thirst.png";
+	}
+	, bind(HUDThirst.load, self))
+	
+	self.m_Active = false
 end
 
 function HUDThirst:drawThis()
-	dxDrawImage(self.m_AbsoluteX, self.m_AbsoluteY, self.m_Width, self.m_Height, "files/images/HUD/Thirst.png", 0, 0, 0, tocolor(255-255*(localPlayer:getThirst()/100), 255*(localPlayer:getThirst()/100), 0))
+	if self.m_Active then
+		dxDrawImage(self.m_AbsoluteX, self.m_AbsoluteY, self.m_Width, self.m_Height, "files/images/HUD/Thirst.png", 0, 0, 0, tocolor(255-255*(localPlayer:getThirst()/100), 255*(localPlayer:getThirst()/100), 0))
+	end
+end
+
+function HUDThirst:load()
+	self.m_Active = true
 end
